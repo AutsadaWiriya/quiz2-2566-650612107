514d65afe1a99384455680b2899a40da4e781a75
Autsada Wiriya <aut0935261784@gmail.com>
2023-08-10 14:15:03 +0700

    Finish

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -6,9 +8,35 @@ export const Comment = ({
   replies,
 }) => {
   return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
-    </div>
+    <>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{commentText}</span>
+          {likeNum > 0 && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{likeNum} คน</span>
+            </div>
+          )}
+        </div>
+      </div>
+      {
+        replies.map( (reply) => 
+          <Reply userImagePath={reply.userImagePath} username={reply.username} likeNum={reply.likeNum} replyText={reply.replyText} key={reply.id}/>
+        )
+      }
+    </>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,28 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+export const PostOwner = ({ userName, studentId, post, numLike, myimg }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src={myimg}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          {userName} {studentId}
+        </span>
+      </div>
+
+      <span>{post}</span>
+
+      { numLike > 0 &&
+        <div className="d-flex align-items-center gap-1">
+          <img src="/like.svg" width={20}></img>
+          <span className="text-muted">{numLike} คน</span>
+        </div>
+      }
+      <hr className="m-0 border" />
+    </div>
+  );
 };
